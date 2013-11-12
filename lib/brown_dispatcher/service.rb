9ccce1e3eef9672c9e95c7d5fa27d7dee488b9ad
ciscou <francismpp@gmail.com>
2013-11-12 14:35:49 +0100

    can enable/disable services

--- lib/brown_dispatcher/service.rb
+++ lib/brown_dispatcher/service.rb
@@ -10,17 +10,51 @@ module BrownDispatcher
 
     def self.register(hostname, *prefixes)
       prefixes.each do |prefix|
-        Redis.current.hset("brown-dispatcher-services", prefix, hostname)
+        Redis.current.hset("brown-dispatcher-services:#{prefix}", "hostname", hostname)
+        Redis.current.hset("brown-dispatcher-services:#{prefix}", "enabled", true)
+      end
+    end
+
+    def enable
+      self.class.redis_keys_for_hostname(hostname).each do |k|
+        Redis.current.hset(k, "enabled", true)
+      end
+    end
+
+    def disable
+      self.class.redis_keys_for_hostname(hostname).each do |k|
+        Redis.current.hset(k, "enabled", false)
       end
     end
 
     def self.find_for_request_path(request_path)
+      if key = redis_key_for(request_path)
+        hostname = Redis.current.hget(key, "hostname")
+        new(hostname)
+      end
+    end
+
+    private
+
+    def self.redis_key_for(request_path)
       request_path = request_path.dup
       request_path << "/" unless request_path.end_with? "/"
 
-      if prefix = Redis.current.hkeys("brown-dispatcher-services").detect { |k| request_path.start_with? "#{k}/" }
-        hostname = Redis.current.hget("brown-dispatcher-services", prefix)
-        new(hostname)
+      redis_keys.detect do |k|
+        next unless Redis.current.hget(k, "enabled") == "true"
+
+        prefix = k.sub %r{^brown-dispatcher-services:}, ""
+        request_path.start_with? "#{prefix}/"
+      end
+    end
+
+    def self.redis_keys
+      Redis.current.keys("brown-dispatcher-services:*")
+    end
+
+    def self.redis_keys_for_hostname(hostname)
+      redis_keys.select do |k|
+        Redis.current.hget(k, "hostname") == hostname
       end
     end
   end
--- spec/brown_dispatcher/service_spec.rb
+++ spec/brown_dispatcher/service_spec.rb
@@ -7,17 +7,40 @@ describe BrownDispatcher::Service do
   end
 
   describe "#find_for_request_path" do
-    before { expect(Redis.current).to receive(:hkeys).with("brown-dispatcher-services").and_return(%w[/foo]) }
+    before do
+      expect(Redis.current).to receive(:keys).with("brown-dispatcher-services:*").and_return(["brown-dispatcher-services:/bar", "brown-dispatcher-services:/foo", "brown-dispatcher-services:/baz"])
+      expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/bar", "enabled").and_return("true")
+    end
 
     describe "when no service matches requested path" do
+      before do
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/foo", "enabled").and_return("true")
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/baz", "enabled").and_return("true")
+      end
+
       it "should return nil" do
         BrownDispatcher::Service.find_for_request_path("/fuzz/bizz").should be_nil
       end
     end
 
-    describe "when a service matches requested path" do
+    describe "when a service matches requested path but it is disabled" do
+      before do
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/foo", "enabled").and_return("false")
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/baz", "enabled").and_return("true")
+      end
+
       it "should return new Service with appropiate hostname" do
-        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services", "/foo").and_return("http://foobar.io")
+        BrownDispatcher::Service.find_for_request_path("/fuzz/bizz").should be_nil
+      end
+    end
+
+    describe "when a service matches requested path and it is enabled" do
+      before do
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/foo", "enabled").and_return("true")
+      end
+
+      it "should return new Service with appropiate hostname" do
+        expect(Redis.current).to receive(:hget).with("brown-dispatcher-services:/foo", "hostname").and_return("http://foobar.io")
         BrownDispatcher::Service.find_for_request_path("/foo/bar").hostname.should == "http://foobar.io"
       end
     end
@@ -26,7 +49,8 @@ describe BrownDispatcher::Service do
   describe "#register" do
     it "should store all the prefixes in redis" do
       %w[/foo /bar].each do |prefix|
-        expect(Redis.current).to receive(:hset).with("brown-dispatcher-services", prefix, "http://foobar.io")
+        expect(Redis.current).to receive(:hset).with("brown-dispatcher-services:#{prefix}", "hostname", "http://foobar.io")
+        expect(Redis.current).to receive(:hset).with("brown-dispatcher-services:#{prefix}", "enabled", true)
       end
       BrownDispatcher::Service.register("http://foobar.io", "/foo", "/bar")
     end
