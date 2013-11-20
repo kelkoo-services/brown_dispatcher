4d963843397d13531aaa7c3e5cc78e24c82e52c9
ciscou <francismpp@gmail.com>
2013-11-20 15:33:21 +0100

    add user_auth if present

--- lib/brown_dispatcher/dispatcher.rb
+++ lib/brown_dispatcher/dispatcher.rb
@@ -9,9 +9,9 @@ module BrownDispatcher
     def dispatch(env)
       uri = "#{@service.hostname}#{@request_path}"
       @res = if env["REQUEST_METHOD"] == "POST"
-               HTTParty.post uri, body: env["rack.request.form_hash"]
+               HTTParty.post uri, body: post_params_for(env)
              else
-               HTTParty.get uri, query: env["QUERY_STRING"]
+               HTTParty.get uri, query: get_params_for(env)
             end
     end
 
@@ -19,6 +19,28 @@ module BrownDispatcher
       headers = @res.headers
       headers.delete("transfer-encoding")
       [ @res.code, headers, [ @res.body ] ]
+    end
+
+    private
+
+    def get_params_for(env)
+      query_string = env["QUERY_STRING"] || ""
+      params = CGI.parse(query_string)
+      merge_user_auth! env, params
+      URI.encode_www_form(params)
+    end
+
+    def post_params_for(env)
+      params = env["rack.request.form_hash"] || {}
+      merge_user_auth! env, params
+      params
+    end
+
+    def merge_user_auth!(env, params)
+      signed_cookies = env["action_dispatch.cookies"].signed
+      user_auth = signed_cookies[:user_auth]
+
+      params.update(user_auth: user_auth) if user_auth
     end
   end
 end
--- spec/brown_dispatcher/dispatcher_spec.rb
+++ spec/brown_dispatcher/dispatcher_spec.rb
@@ -4,32 +4,81 @@ describe BrownDispatcher::Dispatcher do
   let(:env) do
     {
       "REQUEST_METHOD" => "GET",
-      "REQUEST_PATH" => "/foo/bar"
+      "REQUEST_PATH" => "/foo/bar",
+      "action_dispatch.cookies" => double("Cookies", signed: {})
     }
   end
   let(:service) { double("Service", hostname: "http://foobar.io") }
   let(:request_path) { env["REQUEST_PATH"] }
 
   describe "get requests" do
-    before { env.update("QUERY_STRING" => "foo=bar") }
+    describe "without params or user_auth" do
+      it "should delegate to the service" do
+        FakeWeb.register_uri :get, "http://foobar.io/foo/bar", body: "get response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "get response from http://foobar.io/foo/bar" ] ]
+      end
+    end
 
-    it "should delegate to the service" do
-      FakeWeb.register_uri :get, "http://foobar.io/foo/bar?foo=bar", body: "get response from http://foobar.io/foo/bar"
-      dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
-      dispatcher.dispatch(env)
-      dispatcher.to_rack_result.should == [ 200, {}, [ "get response from http://foobar.io/foo/bar" ] ]
+    describe "with params" do
+      before { env.update("QUERY_STRING" => "foo=bar") }
+
+      it "should delegate to the service" do
+        FakeWeb.register_uri :get, "http://foobar.io/foo/bar?foo=bar", body: "get response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "get response from http://foobar.io/foo/bar" ] ]
+      end
+    end
+
+    describe "with user_auth" do
+      before { env["action_dispatch.cookies"].signed.update(user_auth: "1234") }
+
+      it "should delegate to the service" do
+        FakeWeb.register_uri :get, "http://foobar.io/foo/bar?user_auth=1234", body: "get response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "get response from http://foobar.io/foo/bar" ] ]
+      end
     end
   end
 
   describe "post requests" do
-    before { env.update("REQUEST_METHOD" => "POST", "rack.request.form_hash" => { "foo" => "bar" }) }
+    before { env.update("REQUEST_METHOD" => "POST") }
 
-    it "should delegate to the service" do
-      FakeWeb.register_uri :post, "http://foobar.io/foo/bar", body: "post response from http://foobar.io/foo/bar"
-      dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
-      dispatcher.dispatch(env)
-      dispatcher.to_rack_result.should == [ 200, {}, [ "post response from http://foobar.io/foo/bar" ] ]
-      FakeWeb.last_request.body.should == "foo=bar"
+    describe "without params or user_auth" do
+      it "should delegate to the service" do
+        FakeWeb.register_uri :post, "http://foobar.io/foo/bar", body: "post response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "post response from http://foobar.io/foo/bar" ] ]
+        FakeWeb.last_request.body.should == ""
+      end
+    end
+
+    describe "with params" do
+      before { env.update("rack.request.form_hash" => { "foo" => "bar" }) }
+
+      it "should delegate to the service" do
+        FakeWeb.register_uri :post, "http://foobar.io/foo/bar", body: "post response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "post response from http://foobar.io/foo/bar" ] ]
+        FakeWeb.last_request.body.should == "foo=bar"
+      end
+    end
+
+    describe "with user_auth" do
+      before { env["action_dispatch.cookies"].signed.update(user_auth: "1234") }
+
+      it "should delegate to the service" do
+        FakeWeb.register_uri :post, "http://foobar.io/foo/bar", body: "post response from http://foobar.io/foo/bar"
+        dispatcher = BrownDispatcher::Dispatcher.new(service, request_path)
+        dispatcher.dispatch(env)
+        dispatcher.to_rack_result.should == [ 200, {}, [ "post response from http://foobar.io/foo/bar" ] ]
+        FakeWeb.last_request.body.should == "user_auth=1234"
+      end
     end
   end
 end
